4ae02fbb29db53af9bcd18d1e8dd33521f466db1
Dark Sun <dacom.dark.sun@gmail.com>
2020-09-08 13:01:55 +0500

    fix

--- include/consts.hpp
+++ include/consts.hpp
@@ -15,7 +15,7 @@ namespace eosio {
   static constexpr uint64_t _MAX_LEVELS = 7;
 
   static constexpr eosio::name _me = "core.y"_n;
-  static constexpr eosio::name _partners = "partners"_n;
+  static constexpr eosio::name _partners = "part"_n;
   static constexpr eosio::name _curator = "curator"_n;
   static constexpr eosio::name _saving = "eosio.saving"_n;
   
