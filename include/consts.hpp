67838d4a3cae80eb58143803eafe556022311dcf
Dark Sun <dacom.dark.sun@gmail.com>
2021-07-07 14:36:18 +0300

    consts

--- include/consts.hpp
+++ include/consts.hpp
@@ -6,7 +6,7 @@
   #define TOTAL_SEGMENTS 1000000000
   #define MAX_CORE_HISTORY_LENGTH 1000
 
-  static constexpr eosio::symbol _SYM     = eosio::symbol(eosio::symbol_code("UNIT"), 4);
+  static constexpr eosio::symbol _SYM     = eosio::symbol(eosio::symbol_code("FLO"), 4);
   static constexpr eosio::symbol _POWER     = eosio::symbol(eosio::symbol_code("POWER"), 0);
 
   static constexpr uint64_t _DATA_ORDER_EXPIRATION = 86400;
@@ -14,12 +14,12 @@
   static constexpr uint64_t _TOTAL_VOTES = 7;
   static constexpr uint64_t _MAX_LEVELS = 7;
 
-  static constexpr eosio::name _me = "core.y"_n;
+  static constexpr eosio::name _me = "unicore"_n;
   static constexpr eosio::name _partners = "part"_n;
   static constexpr eosio::name _curator = "curator"_n;
   static constexpr eosio::name _registrator = "registrator"_n;
   static constexpr eosio::name _gateway = "gateway"_n;
   static constexpr eosio::name _saving = "eosio.saving"_n;
-  static constexpr eosio::name _core_host = "core"_n;
+  static constexpr eosio::name _core_host = "community"_n;
 
 // }
